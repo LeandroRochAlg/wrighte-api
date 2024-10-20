4efdb80133b36884e05d4eba96c91124cedac0f7
LeandroRochAlg <leandrochaju@hotmail.com>
2024-10-20 04:28:32 -0300

    build: Adicionar .env para gerenciar variáveis de ambiente

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,16 @@
 import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Configuração de conexão
 const pgp = pgPromise();
 const db = pgp({
-  host: 'localhost',
-  port: 5432,
-  database: 'WrightE',
-  user: 'postgres',
-  password: '1212'
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432, // Convertendo para inteiro
+  database: process.env.DB_NAME || 'postgres',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
 });
 
 export default db;
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import bcrypt from "bcrypt";
 import db from "./config/database";
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
