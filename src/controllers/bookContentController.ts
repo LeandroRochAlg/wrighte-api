2982d517b81f0be9dfd178cf46c80bcb5a0cb725
Pedro Luiz <pedrin.luiz22@gmail.com>
2024-10-21 12:41:42 -0300

    adicao no banco de dados a coluna title para o livro e ajustes no back-end

--- src/controllers/bookContentController.ts
+++ src/controllers/bookContentController.ts
@@ -3,18 +3,18 @@ import db from '../config/database';
 
 
 class bookContentController{
-    public async bookContent(req: Request, res: Response){
-    const { content } = req.body;
+    public async bookContent(req: Request, res: Response) {
+        const { title, content } = req.body; // Receber o título e o conteúdo
 
-    try {
-        // Insira seu código para salvar o conteúdo no banco de dados
-        await db.none('INSERT INTO contents(content) VALUES($1)', [content]);
+        try {
+            // Insira o título e o conteúdo no banco de dados
+            await db.none('INSERT INTO contents(title, content) VALUES($1, $2)', [title, content]);
 
-        res.status(200).json({ message: 'Conteúdo salvo com sucesso' });
-    } catch (error) {
-        console.error('Erro ao salvar conteúdo:', error);
-        res.status(500).json({ message: 'Erro ao salvar conteúdo' });
-    }
+            res.status(200).json({ message: 'Conteúdo salvo com sucesso' });
+        } catch (error) {
+            console.error('Erro ao salvar conteúdo:', error);
+            res.status(500).json({ message: 'Erro ao salvar conteúdo' });
+        }
     }
 }
 
